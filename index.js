64bc1d883c75814d49b931cbe8775c364560e526
Manish Kumar <108109935+ManishK4514@users.noreply.github.com>
2023-10-18 01:07:32 +0530

    initial Commit to push to the github

---
+++ index.js
@@ -0,0 +1,33 @@
+import TelegramBot from "node-telegram-bot-api";
+import axios from "axios";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const token = "6354816427:AAFbMfV6v3yJlEX7ijiAlLyBMqZrYNRJCHk";
+
+const bot = new TelegramBot(token, { polling: true });
+
+bot.on("message", async(msg) => {
+    const chatId = msg.chat.id;
+    const userInput = msg.text;
+
+    try {
+        const response = await axios.get(
+          `https://api.openweathermap.org/data/2.5/weather?q=${userInput}&appid=${process.env.WEATHER_API_KEY}`
+        );
+
+        const data = response.data;
+        const weather = data.weather[0].description;
+        const temperature = data.main.temp - 273.15;
+        const city = data.name;
+        const humidity = data.main.humidity;
+        const pressure = data.main.pressure;
+        const windSpeed = data.wind.speed;
+        const message = `The weather in ${city} is ${weather} with a temperature of ${temperature.toFixed(2)}°C. The humidity is ${humidity}%, the pressure is ${pressure}hPa, and the wind speed is ${windSpeed}m/s.`;
+    
+        bot.sendMessage(chatId, message);
+      } catch (error) {
+        bot.sendMessage(chatId, "City doesn't exist.");
+      }
+});
